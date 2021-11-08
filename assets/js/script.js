dc15432781a5b5fc119b11ab0edb8df392892985
CarusCha <caruscha.hk@gmail.com>
2021-11-08 08:25:15 +0800

    adding time

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,22 +14,29 @@ htmlText += "</div>";
 // close left nav
 
 // add contents
+var totalSec = 0;
 htmlText += `<div class="tab-content contentView" id="v-pills-tabContent">`;
 for (const [sec_num, chapter] of json.entries()) {
+    var totalChapterSec = 0;
     let isActive = sec_num == 0;
     htmlText += `<div class="tab-pane fade${isActive ? " active show" : ""}" id="nav-${sec_num}" role="tabpanel" aria-labelledby="nav-${sec_num}-tab">`;
     htmlText += `<div class="accordion accordion-flush" id="accordionFlushExample${sec_num}">`
     for (const [lec_num, lecture] of chapter["lecture"].entries()) {
+        let time = lecture["time"];
+        let times = time.split(':');
+        totalChapterSec += parseInt(times[0] * 60);
+        totalChapterSec += parseInt(times[1]);
+
         htmlText += `
             <div class="accordion-item">
                 <h2 class="accordion-header" id="flush-heading${sec_num}_${lec_num}">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${sec_num}_${lec_num}" aria-expanded="false" aria-controls="flush-collapse${sec_num}_${lec_num}">
-                        ${lecture["text"]} (${lecture["time"]})
+                        ${lecture["text"]} (${time})
                     </button>
                 </h2>
                 <div id="flush-collapse${sec_num}_${lec_num}" class="accordion-collapse collapse" aria-labelledby="flush-heading${sec_num}_${lec_num}" data-bs-parent="#accordionFlushExample${sec_num}">
                     <div class="accordion-body iframe_container">
-                        <iframe src="${lecture["url"]}" allowfullscreen title="${lecture["text"]} (${lecture["time"]})"></iframe>
+                        <iframe src="${lecture["url"]}" allowfullscreen title="${lecture["text"]} (${time})"></iframe>
                     </div>
                 </div>
             </div>
@@ -39,6 +46,8 @@ for (const [sec_num, chapter] of json.entries()) {
     htmlText += "</div>";
     htmlText += "</div>";
 }
+// htmlText += `<p>${totalChapterSec}</p>`;
+
 htmlText += "</div>";
 // close contents
 
