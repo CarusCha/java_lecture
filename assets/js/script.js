8af34278178d3e1d3280c3a316c53aca12a31c75
CarusCha <carus.cha@eats365.com>
2021-11-08 17:05:35 +0800

    add cookie

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,68 @@
-// import json file
-import json from "../../data/java_lecture.json" assert { type: "json" };
+// cookie
+function setCookie(key, value, exdays) {
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var expires = (exdays == null) ? "" : `; expires=${exdate.toUTCString()}`
+    var cookie = `${key}=${escape(value)}${expires}`
+    document.cookie = cookie;
+ }
+ 
+ function getCookie(key) {
+     const value = `; ${document.cookie}`;
+     const parts = value.split(`; ${key}=`);
+     if (parts.length === 2) return parts.pop().split(';').shift();
+ }
 
-var htmlText = `<div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical" style="width:120px;">`;
+ function setHTML() {
+    // import json file
+    import json from "../../data/java_lecture.json" assert { type: "json" };
 
-// add left nav
-for (const [i, chapter] of json.entries()) {
-    let isActive = i == 0;
-    htmlText += `<button class="nav-link${isActive ? " active" : ""}" id="nav-${i}-tab" data-bs-toggle="tab" data-bs-target="#nav-${i}" type="button" role="tab" aria-controls="nav-${i}" aria-selected="${isActive ? "true" : "false"}">
-    ${chapter["name"]}
-    </button>`;
-}
-htmlText += "</div>";
-// close left nav
+    var htmlText = `<div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical" style="width:120px;">`;
 
-// add contents
-htmlText += `<div class="tab-content contentView" id="v-pills-tabContent">`;
-for (const [sec_num, chapter] of json.entries()) {
-    let isActive = sec_num == 0;
-    htmlText += `<div class="tab-pane fade${isActive ? " active show" : ""}" id="nav-${sec_num}" role="tabpanel" aria-labelledby="nav-${sec_num}-tab">`;
-    htmlText += `<div class="accordion accordion-flush" id="accordionFlushExample${sec_num}">`
-    for (const [lec_num, lecture] of chapter["lecture"].entries()) {
-        htmlText += `
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="flush-heading${sec_num}_${lec_num}">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${sec_num}_${lec_num}" aria-expanded="false" aria-controls="flush-collapse${sec_num}_${lec_num}">
-                        ${lecture["text"]} (${lecture["time"]})
-                    </button>
-                </h2>
-                <div id="flush-collapse${sec_num}_${lec_num}" class="accordion-collapse collapse" aria-labelledby="flush-heading${sec_num}_${lec_num}" data-bs-parent="#accordionFlushExample${sec_num}">
-                    <div class="accordion-body iframe_container">
-                        <iframe src="${lecture["url"]}" allowfullscreen title="${lecture["text"]} (${lecture["time"]})"></iframe>
+    // add left nav
+    for (const [i, chapter] of json.entries()) {
+        let isActive = i == 0;
+        htmlText += `<button class="nav-link${isActive ? " active" : ""}" id="nav-${i}-tab" data-bs-toggle="tab" data-bs-target="#nav-${i}" type="button" role="tab" aria-controls="nav-${i}" aria-selected="${isActive ? "true" : "false"}">
+        ${chapter["name"]}
+        </button>`;
+    }
+    htmlText += "</div>";
+    // close left nav
+
+    // add contents
+    htmlText += `<div class="tab-content contentView" id="v-pills-tabContent">`;
+    for (const [sec_num, chapter] of json.entries()) {
+        let isActive = sec_num == 0;
+        htmlText += `<div class="tab-pane fade${isActive ? " active show" : ""}" id="nav-${sec_num}" role="tabpanel" aria-labelledby="nav-${sec_num}-tab">`;
+        htmlText += `<div class="accordion accordion-flush" id="accordionFlushExample${sec_num}">`
+        for (const [lec_num, lecture] of chapter["lecture"].entries()) {
+            htmlText += `
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-heading${sec_num}_${lec_num}">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${sec_num}_${lec_num}" aria-expanded="false" aria-controls="flush-collapse${sec_num}_${lec_num}">
+                            ${lecture["text"]} (${lecture["time"]})
+                        </button>
+                    </h2>
+                    <div id="flush-collapse${sec_num}_${lec_num}" class="accordion-collapse collapse" aria-labelledby="flush-heading${sec_num}_${lec_num}" data-bs-parent="#accordionFlushExample${sec_num}">
+                        <div class="accordion-body iframe_container">
+                            <iframe src="${lecture["url"]}" allowfullscreen title="${lecture["text"]} (${lecture["time"]})"></iframe>
+                        </div>
                     </div>
                 </div>
-            </div>
-        `;
+            `;
+        }
+        // close tab
+        htmlText += "</div>";
+        htmlText += "</div>";
     }
-    // close tab
     htmlText += "</div>";
-    htmlText += "</div>";
-}
-htmlText += "</div>";
-// close contents
+    // close contents
 
-document.querySelector("#left_nav").innerHTML = htmlText;
+    document.querySelector("#left_nav").innerHTML = htmlText;
+
+ }
+
+
+
+ // run
+ setHTML();
